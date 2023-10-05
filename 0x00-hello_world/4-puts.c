a8189c513a430fd7abacb2dded74d765df54aae7
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-10-05 15:30:02 -0700

    New Task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stido.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A c program that print with put function
+ *
+ * retun : always 0 (Success )
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
