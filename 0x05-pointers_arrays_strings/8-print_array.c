db6017a393864885dc6bd8d9a27948df65b48ecd
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-10-16 13:56:00 -0700

    newline

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,8 +3,8 @@
 /**
  * print_array - prints n elements of an array of integers
  *
- * @n: elements paramter input
- * @a: string parameter input
+ * %n: elements paramter input
+ * %a: string parameter input
  *
  * Retun: nothing
  */
@@ -20,5 +20,5 @@ void print_array(int *a, int n)
 		else
 			print("%d", a[i]);
 	}
-	print("\n");
+	printf("\n");
 }
