9ae89bf4c2ed8a0ccd1da07322c76e04f4f45f17
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-11-09 17:19:35 -0800

    add

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -39,10 +39,10 @@ void format_string(char *separator, va_list ap)
 {
 	char *str = va_arg(ap, char *);
 
-	switch ((int)(!str)) {
+	switch ((int)(!str))
 		case 1:
 			str = "(nil)";
-	}
+	
 	printf("%s%s", separator, str);
 }
 
