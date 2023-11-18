d1a3796a77fb59501ddfececc30de4eec208f632
ahmedalaeldin <alaa.mido515@yahoo.com>
2023-11-17 16:22:45 -0800

    newfiles

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+nclude "main.h"
+
+/**
+ * _print_rev_recursion - prints string in revese
+ * @s: pointer to string
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s)
+	}
+}
